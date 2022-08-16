63674c4da285123eb95a4d57f329437e1bf615f2
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2022-08-16 11:17:51 -0600

    feat/use fragment, argument args, deep links

--- app/src/main/java/com/fernando/curso/MainActivity.kt
+++ app/src/main/java/com/fernando/curso/MainActivity.kt
@@ -6,38 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        text = findViewById<TextView>(R.id.txt_output)
-        val button = findViewById<Button>(R.id.btn_accion)
-
-        button.setOnClickListener {
-            navigationSecondActivity()
-        }
-
-    }
-
-    private fun navigationSecondActivity() {
-        val intent = Intent(this, SegundaActivity::class.java)
-        intent.putExtra("name", "Kotlin is beautiful!")
-        startActivityForResult(intent, 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1) {
-            if (resultCode == Activity.RESULT_OK) {
-                val name = data?.getStringExtra("name2")
-                text.text = name
-            }
-        }
     }
 
 }
