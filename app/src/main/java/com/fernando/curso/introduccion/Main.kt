9aaafbaea5142f23b3307166f125c930f1e6632a
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-04-13 10:14:56 -0600

    Nulabilidad en Kotlin

--- app/src/main/java/com/fernando/curso/introduccion/Main.kt
+++ app/src/main/java/com/fernando/curso/introduccion/Main.kt
@@ -14,6 +14,28 @@ fun main() {
 
     println("Remover primer caracter: ${nombre.removerPrimerCaracter()}")
 
+    mainValuesNull()
+
 }
 
 fun String.removerPrimerCaracter() : String = this.substring(1)
+
+
+/**
+ * Nulabilidad en kotlin
+ */
+fun mainValuesNull() {
+
+    val nombre: String? = null
+
+    // Elvis operator
+    //val nombre2 = nombre?.length ?: "Leo".length
+
+    nombre?.let {
+        println(it)
+    }
+
+    // !! = Le digo explicitamente que no va ser null
+    //println(nombre2)
+
+}
