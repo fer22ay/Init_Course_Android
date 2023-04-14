0ca912babe5b6897178885f8783be34d8344fb24
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-04-14 10:32:43 -0600

    Breve introducción a Jetpack Compose, creación de calculadora para testear sus diferentes funcionalidades y complejidad

--- app/src/main/java/com/fernando/curso/MainActivity.kt
+++ app/src/main/java/com/fernando/curso/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.fernando.curso
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.commit
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fernando.curso.ui.theme.CalculatorPrepTheme
+import com.fernando.curso.ui.theme.MediumGray
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CalculatorPrepTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp),
+                    buttonSpacing = buttonSpacing,
+                    onAction = viewModel::onAction
+                )
+            }
+        }
     }
 
 }
